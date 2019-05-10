0c95e6f2f008a9b10095d057ce1591b7af071746
Zach Overhulser <zoverhulser@gmail.com>
2019-05-10 17:15:52 +0000

    Completes routes

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const helmet = require("helmet");
-const projectRouter = require("./data/helpers/projectModel");
+const projectRouter = require("./routes/projects");
+const actionRouter = require("./routes/actions");
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 server.use("/api/projects", projectRouter);
+server.use("/api/actions", actionRouter);
 
 server.get("/", async (req, res) => {
 	res.send(`
